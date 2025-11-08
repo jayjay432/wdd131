242be2f9f6dbc798fbac5611fae794a249a0d42b
jayjay432 <lawrencejoseph2859@gmail.com>
2025-11-08 13:48:43 +0100

    correction to the javascript file

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,9 @@
 
 const lastModified = document.querySelector("#lastModified");
+const copyWriteYear = document.querySelector("#copy-write-year");
 
 const today = new Date();
 
 
 lastModified.innerHTML = today;
+copyWriteYear.innerHTML = today.getFullYear();
